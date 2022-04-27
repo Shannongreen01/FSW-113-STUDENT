417cba22c463cfe4e05095943c9afa4f318d88c0
Shannongreen01 <shannon.green@bryanuniversity.edu>
2022-04-27 11:03:20 -0400

    added code

--- Week-2/calendar.js
+++ Week-2/calendar.js
@@ -1,14 +1,11 @@
-
-document.getElementById('enter').addEventListener('click', print);
-function print(){
+document.getElementById('go').addEventListener('click', printCalendar);
+function printCalendar(){
     var selectedYear = document.getElementById('year');
     var selectedMonth = document.getElementById('month');
    
     selectedYear = parseInt(selectedYear.value);
-
+   
     selectedMonth = parseInt(selectedMonth.value);
- 
-
 
 const today = new Date(`${selectedMonth + 1}/1/${selectedYear}`);
 
@@ -29,32 +26,31 @@ switch (month) {
         days = 31
 };
 
-let calDay = document.getElementById('calDay');
-calDay.innerHTML = ' ';
+let calendarDays = document.getElementById('calendarDays');
+calendarDays.innerHTML = ' ';
 let x;
 const weekday = today.getDay()
 
 for (x = 0; x < weekday; x++){
-    calDay.innerHTML += "<div class='blankDay'>&nbsp;</div>";
+    calendarDays.innerHTML += "<div class='blankDay'>&nbsp;</div>";
 };
 
 let dt = 0;
 do {
     dt++;
-    calDay.innerHTML += `<div class='calDiv'>${dt}</div`;
+    calendarDays.innerHTML += `<div class='calendarCell'>${dt}</div`;
 } while ( dt < days);
 
-const name = today.toLocaleDateString('default', {month:'long'});
+const monthName = today.toLocaleDateString('default', {month:'long'});
 const year = today.getFullYear();
-document.querySelector('.title').innerText = `${name} ${year}`;
+document.querySelector('.calendarTitle').innerText = `${monthName} ${year}`;
 const remainder = (7 - ((x + dt) % 7));
 let y = 0;
 while ( y < remainder) {
-    calDay.innerHTML += "<div class='blankDay'>&nbsp;</div>";
+    calendarDays.innerHTML += "<div class='blankDay'>&nbsp;</div>";
     y++;
 }; 
 };
-
 
 let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 let select = document.getElementById('month');
@@ -64,4 +60,4 @@ for (let i = 0; i < months.length; i++){
     newOptions.setAttribute('value', months.indexOf(months[i]));
     select.append(newOptions);
 };
-print();
+printCalendar();
